9b9bf479fa963ba7457db45849ddc5a6b419b6d4
Junil Ko <junil@skkukdp.re.kr>
2024-01-09 14:28:18 +0900

    OfficeTableViewCell 추가

---
+++ Assets/Scripts/New Office/OfficeTableViewController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class OfficeTableViewController : GOTableViewController
+{
+    [SerializeField] private string serviceKey;
+
+    private List<Office> officeList = new List<Office>();
+
+    private void Start()
+    {
+        StartCoroutine(LoadData());
+    }
+
+    protected override int numberOfRows()
+    {
+        return officeList.Count;
+    }
+
+    protected override GOTableViewCell cellForRowAtIndex(int index)
+    {
+
+        return null;
+    }
+
+    IEnumerator LoadData()
+    {
+        string url = string.Format("{0}?page={1}&perPage={2}&serviceKey={3}",
+            Constants.url, 0, 100, serviceKey);
+
+        UnityWebRequest request = new UnityWebRequest();
+        using (request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                string result = request.downloadHandler.text;
+
+                OfficeData officeData = JsonUtility.FromJson<OfficeData>(result);
+                Office[] officeArray = officeData.data;
+
+                for (int i = 0; i < officeArray.Length; i++)
+                {
+                    officeList.Add(officeArray[i]);
+                }
+            }
+        }
+    }
+}
